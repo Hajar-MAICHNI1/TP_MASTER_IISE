983db1910ec7d6596f4642d8b399833fb5709ef6
Hajar-MAICHNI1 <hajar.maichni01@gmail.com>
2024-12-26 21:05:57 +0100

    next commit

--- TP5/EX1.py
+++
@@ -1,8 +0,0 @@
-#Ouverture de Fichier
-#fichier=open("TP5/exemple.txt", "r" )
-
-with open("TP5/exemple.txt", "r")as fichier:
-    ligne = fichier.readline()
-    #Affiche chaque ligne du texte avec son numéro
-    for index, ligne in enumerate(fichier, start=1):
-         print(f"{index}: {ligne.strip()}")
